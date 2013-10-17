d0356bd6b59776b42e6bb069aac989413acb3af6
Anton Yabchinskiy <arn@bestmx.ru>
2013-10-17 23:55:33 +0400

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd $(dirname $0)
+for x in \
+    XCompose \
+    Xresources \
+    kshrc \
+    profile \
+    xinitrc
+do
+    if [ -e $HOME/.$x ]; then
+        echo "File $HOME/.$x exists"
+    else
+        ln -s $PWD/$x $HOME/.$x
+    fi
+done
