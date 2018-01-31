589192cbd18b168f1f0439941960e881a84c910f
Anton Yabchinskiy <arn@bestmx.net>
2018-01-31 16:03:53 +0300

    install-fonts.sh: follow redirects

--- install-fonts.sh
+++ install-fonts.sh
@@ -7,6 +7,6 @@ trap "rm -rf $TEMP_FILE" EXIT
 
 BASE_URL=http://www.paratype.ru/uni/public
 for FONT in Sans Serif Mono; do
-    curl $BASE_URL/PT$FONT.zip -o $TEMP_FILE || exit 1
+    curl -L $BASE_URL/PT$FONT.zip -o $TEMP_FILE || exit 1
     unzip -n -x $TEMP_FILE -d ~/.fonts '*.ttf' || exit 1
 done
