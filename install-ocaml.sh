783896e74eb171948bc7be0baa5c77a8a5f16dc0
Anton Yabchinskiy <arn@bestmx.net>
2019-09-19 16:17:23 +0300

    install-ocaml.sh: update packages

--- install-ocaml.sh
+++ install-ocaml.sh
@@ -2,5 +2,13 @@
 
 opam init -n || exit 1
 opam install -y \
-     batteries menhir merlin ocp-indent qtest utop \
+     batteries \
+     menhir \
+     merlin \
+     num \
+     ocamlbuild \
+     ocp-indent \
+     qtest \
+     utop \
+     zarith \
     || exit 1
