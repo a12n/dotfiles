df6ac5a054e3fde9477fe6c795b03cf5cc648280
Anton Yabchinskiy <arn@bestmx.net>
2019-12-24 02:35:04 +0300

    install-ocaml: update vsn, more packages

--- install-ocaml.sh
+++ install-ocaml.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-opam init -n || exit 1
+opam init -n -c 4.08.1 || exit 1
 opam install -y \
+     base \
      batteries \
+     core_bench \
      menhir \
      merlin \
      num \
      ocamlbuild \
      ocp-indent \
      qtest \
+     stdio \
      utop \
      zarith \
     || exit 1
